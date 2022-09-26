223b995eaa01b2f3422058d342da9533fd4d76bc
Mahdipourarasteh <mahdi.mahdipourarasteh@gmail.com>
2022-09-27 00:04:36 +0200

    new commit

--- Uber-Trips-Analysis/Uber-Trips-Analysis.py
+++ Uber-Trips-Analysis/Uber-Trips-Analysis.py
@@ -1,2 +1,17 @@
-import numpy as np
-import pandas as pd 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+uber_df= pd.read_csv("uber-raw-data-sep14.csv")
+#print(uber_df.head(5))
+#print(uber_df.tail())
+#print(uber_df.shape)
+#print(uber_df.info())
+
+#Change the "Date/Time" column's data type from string to datetime
+uber_df['Date/Time']= pd.to_datetime(uber_df['Date/Time'])
+
+uber_df["Day"]= uber_df['Date/Time'].apply(lambda x: x.day)
+uber_df["Hour"]= uber_df['Date/Time'].apply(lambda x: x.hour)
+uber_df["Weekday"]= uber_df['Date/Time'].apply(lambda x: x.weekday())
+#print(uber_df.head(5))
+
