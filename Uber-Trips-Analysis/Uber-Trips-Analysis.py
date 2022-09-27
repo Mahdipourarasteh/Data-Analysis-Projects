2a9898e510be564912f9ce464ef63b9a9404d13c
Mahdipourarasteh <mahdi.mahdipourarasteh@gmail.com>
2022-09-28 00:18:24 +0200

    new commit

--- Uber-Trips-Analysis/Uber-Trips-Analysis.py
+++ Uber-Trips-Analysis/Uber-Trips-Analysis.py
@@ -15,3 +15,38 @@ uber_df["Hour"]= uber_df['Date/Time'].apply(lambda x: x.hour)
 uber_df["Weekday"]= uber_df['Date/Time'].apply(lambda x: x.weekday())
 #print(uber_df.head(5))
 
+#visualize the density of rides per Day of month
+fig1,ax = plt.subplots(figsize= (12,6))
+plt.hist(uber_df.Day, width= 0.6, bins= 30)
+plt.title("Density of trips per Day", fontsize=16)
+plt.xlabel("Day", fontsize=14)
+plt.ylabel("Density of rides", fontsize=14)
+fig1.show()
+
+#Visualize the Density of rides per Weekday
+fig2,ax = plt.subplots(figsize = (12,6))
+plt.hist(uber_df.Weekday, width= 0.6, range= (0, 6.5), bins=7, color= "green")
+plt.title("Density of trips per Weekday", fontsize=16)
+plt.xlabel("Weekday", fontsize=14)
+plt.ylabel("Density of rides", fontsize=14)
+fig2.show()
+
+#Visualize the Density of rides per hour
+fig3,ax = plt.subplots(figsize = (12,6))
+plt.hist(uber_df.Hour, width= 0.6, bins=24, color= "orange")
+plt.title("Density of trips per Hour", fontsize=16)
+plt.xlabel("Hour", fontsize=14)
+plt.ylabel("Density of rides", fontsize=14)
+fig3.show()
+
+#Visualize the Density of rides per location
+fig4,ax = plt.subplots(figsize = (12,6))
+x= uber_df.Lon
+y= uber_df.Lat
+plt.scatter(x, y, color= "purple")
+plt.title("Density of trips per Hour", fontsize=16)
+plt.xlabel("Hour", fontsize=14)
+plt.ylabel("Density of rides", fontsize=14)
+fig4.show()
+
+input()
